d55d802279aedff7298df026243bbe8476dbc6c5
Barbara Brina <bmbrina9@gmail.com>
2018-09-17 17:04:28 -0500

    Adds portfolio items via resource generator

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,6 @@
 class PagesController < ApplicationController
   def home
+    @blogs = Blog.all
     @skills = Skill.all
   end
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
+  resources :blogs
+  resources :portfolios
+
   get 'pages/home'
 
   get 'pages/about'
 
   get 'pages/contact'
-
-  resources :blogs
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
