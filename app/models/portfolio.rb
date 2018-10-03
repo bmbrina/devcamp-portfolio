6d60b72e373135f13460c65dafe940d963303ca6
Barbara Brina <bmbrina9@gmail.com>
2018-10-02 19:10:55 -0500

    Integrated technologies model

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -1,5 +1,6 @@
 class Portfolio < ApplicationRecord
   include Placeholder
+  has_many :technologies
   validates_presence_of :title, :body, :main_image, :thumb_image
 
   def self.react
--- db/seeds.rb
+++ db/seeds.rb
@@ -63,3 +63,11 @@ end
 end
 
 puts "9 portfolio items created."
+
+3.times do | technology |
+  Portfolio.last.technologies.create!(
+    name: "Technology #{technology + 1}"
+  )
+end
+
+puts "3 technologies created."
