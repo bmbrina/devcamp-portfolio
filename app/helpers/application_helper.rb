eedf1c33fca869e83469ad6e6add0d390f552155
Barbara Brina <bmbrina9@gmail.com>
2018-10-16 16:37:11 -0500

    Updated master layout styles along with helper methods

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,15 +1,14 @@
 module ApplicationHelper
-  def login_helper
+  def login_helper style
     if current_user.is_a?(GuestUser)
-      (link_to 'Register', new_user_registration_path) +
-      "<br>".html_safe +
-      (link_to 'Login', new_user_session_path)
+      (link_to 'Register', new_user_registration_path, class: style) +
+      (link_to 'Login', new_user_session_path, class: style)
     else
-      link_to 'Logout', destroy_user_session_path, method: :delete
+      link_to 'Logout', destroy_user_session_path, method: :delete, class: style
     end
   end
 
-  def session_helper
+  def source_helper
     if session[:source]
       greeting = "Thanks for visiting me from #{session[:source]}"
     content_tag(:p, greeting, class: "source-greeting")
