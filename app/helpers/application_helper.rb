a24ce380710164a62b1ab191ded00370d93cdcf2
Barbara Brina <bmbrina9@gmail.com>
2018-10-18 16:07:45 -0500

    Fixed login_helper method

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,5 @@
 module ApplicationHelper
-  def login_helper style
+  def login_helper(style = '')
     if current_user.is_a?(GuestUser)
       (link_to 'Register', new_user_registration_path, class: style) +
       (link_to 'Login', new_user_session_path, class: style)
