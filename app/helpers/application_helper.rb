fd9d8be65c417ca8373166ec81e1359c6b4be18a
Barbara Brina <bmbrina9@gmail.com>
2018-10-19 18:42:41 -0500

    Personalized portfolio navigation bar and masthead

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,6 +2,7 @@ module ApplicationHelper
   def login_helper(style = '')
     if current_user.is_a?(GuestUser)
       (link_to 'Register', new_user_registration_path, class: style) +
+      " ".html_safe +
       (link_to 'Login', new_user_session_path, class: style)
     else
       link_to 'Logout', destroy_user_session_path, method: :delete, class: style
