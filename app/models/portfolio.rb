2b5ed89cc316e734a675cea2f19f620eec069ff6
Barbara Brina <bmbrina9@gmail.com>
2018-10-22 15:30:58 -0500

    Added position attribute to portfolio items

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -11,6 +11,10 @@ class Portfolio < ApplicationRecord
     where(subtitle: "React")
   end
 
+  def self.by_position
+    order('position ASC')
+  end
+
   scope :ruby_on_rails, -> { where(subtitle: "Ruby on Rails")}
 
   after_initialize :set_defaults
