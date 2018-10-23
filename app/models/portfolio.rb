7347b0979239353dc207c2f0be5c03228dbbfdfa
Barbara Brina <bmbrina9@gmail.com>
2018-10-22 19:16:29 -0500

    Updated file upload forms and refactored placeholder functionality into view

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -1,11 +1,9 @@
 class Portfolio < ApplicationRecord
-  include Placeholder
-
   has_many :technologies
   accepts_nested_attributes_for :technologies,
                                 reject_if: lambda { | attrs | attrs['name'].blank? }
 
-  validates_presence_of :title, :body, :main_image, :thumb_image
+  validates_presence_of :title, :body
 
   mount_uploader :main_image, PortfolioUploader
   mount_uploader :thumb_image, PortfolioUploader
@@ -19,11 +17,4 @@ class Portfolio < ApplicationRecord
   end
 
   scope :ruby_on_rails, -> { where(subtitle: "Ruby on Rails")}
-
-  after_initialize :set_defaults
-
-  def set_defaults
-    self.main_image ||= Placeholder.image_generator(height: 600, width: 400)
-    self.thumb_image ||= Placeholder.image_generator(height: 350, width: 200)
-  end
 end
