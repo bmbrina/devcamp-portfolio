a5ea7564d84448b993b240026e628647e2adbfd0
Barbara Brina <bmbrina9@gmail.com>
2018-10-25 16:24:24 -0500

    Built tweet parser for the links inside a tweet.

--- app/helpers/pages_helper.rb
+++ app/helpers/pages_helper.rb
@@ -1,2 +1,25 @@
 module PagesHelper
+  def tweet_parser tweet
+    url_regex = %r{
+      \b
+      (
+        (?: [a-z][\w-]+:
+         (?: /{1,3} | [a-z0-9%] ) |
+          www\d{0,3}[.] |
+          [a-z0-9.\-]+[.][a-z]{2,4}/
+        )
+        (?:
+         [^\s()<>]+ | \(([^\s()<>]+|(\([^\s()<>]+\)))*\)
+        )+
+        (?:
+          \(([^\s()<>]+|(\([^\s()<>]+\)))*\) |
+          [^\s`!()\[\]{};:'".,<>?«»“”‘’]
+        )
+      )
+    }ix
+
+    tweet.gsub(url_regex) do | url |
+      "<a href='#{url}' target='_blank'>#{url}</a>"
+    end.html_safe
+  end
 end
