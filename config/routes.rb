b9f892f44169283a127dba66457a1a264d4fba18
Barbara Brina <bmbrina9@gmail.com>
2018-10-28 17:19:25 -0600

    Built out topics views

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :topics, only: [:index, :show]
   resources :comments
   devise_for :users, path: '', path_names: { sign_in: 'login', sign_out: 'logout', sign_up: 'register' }
   resources :portfolios, except: [:show] do
@@ -19,6 +20,6 @@ Rails.application.routes.draw do
   get 'tech-news', to: 'pages#tech_news'
 
   mount ActionCable.server => '/cable'
-  
+
   root to: 'pages#home'
 end
