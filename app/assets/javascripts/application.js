300445164f761a5faf0304deb28e192d6592b303
Barbara Brina <bmbrina9@gmail.com>
2018-10-28 21:45:46 -0600

    Fixed js loading bug

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,7 +11,6 @@
 // about supported directives.
 //
 //= require jquery3
-//= require bootstrap-sprockets
 //= require rails-ujs
 //= require jquery-ui
 //= require html.sortable
@@ -20,3 +19,4 @@
 //= require cable
 //= require turbolinks
 //= require_tree .
+//= require bootstrap-sprockets
